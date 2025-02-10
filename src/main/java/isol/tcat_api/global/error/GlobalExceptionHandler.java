b46c6e315026db3977718d3a045c16298dbf3517
ik <>
2025-02-11 00:37:26 +0900

    httpMessageNotReadableException 예외 처리 핸들링 추가

--- src/main/java/isol/tcat_api/global/error/GlobalExceptionHandler.java
+++ src/main/java/isol/tcat_api/global/error/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package isol.tcat_api.global.error;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.NoHandlerFoundException;
@@ -33,6 +34,18 @@ public class GlobalExceptionHandler {
                 .build();
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    protected ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        log.error(String.valueOf(e));
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .status(400)
+                .code(4001)
+                .message("잘못된 양식의 데이터입니다. ")
+                .build();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
     @ExceptionHandler(Exception.class)
